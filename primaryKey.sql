392128f097d4f2afcbfffd4ddc30d3314d197a58
Glebasta <hlibpoddubchenko@gmail.com>
2024-08-18 17:51:17 +0200

    Primary key

---
+++ primaryKey.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS "messages";
+
+CREATE TABLE IF NOT EXISTS "messages"(
+    "id" bigserial PRIMARY KEY,
+    "body" varchar(2048) NOT NULL CHECK("body"!=''),
+    "author" varchar(32) NOT NULL CHECK("author" !=''),
+    "createAt" timestamp NOT NULL DEFAULT current_timestamp,
+    "IsREad" boolean  DEFAULT false
+);
+
+INSERT INTO "messages"("body","author")
+VALUES
+('hi','Tom'),
+('hi','Tom'),
+('1111','22222');
